ea0051a4b379bea46bdd10e0d9529e762e1a33cc
심승용 <seungyongshim@nexon.co.kr>
2022-01-03 09:03:18 +0900

    수정

--- src/BlazorApp2/ViewInitialized.cs
+++
@@ -1,2 +0,0 @@
-namespace BlazorApp2;
-public record ViewInitialized;
--- src/Sample.Wasm/Program.cs
+++ src/Sample.Wasm/Program.cs
@@ -1,19 +1,11 @@
-using BlazorApp2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Proto;
+using Sample.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-var system = new ActorSystem();
-var context = system.Root;
-var props = Props.FromProducer(() => new CounterActor());
-var pid = context.SpawnNamed(props, "CounterActor");
-
-builder.Services.AddSingleton(context);
 
 await builder.Build().RunAsync();
