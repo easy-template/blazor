f3f27742fdfcbb3546413f2d631334fac7356d54
심승용 <seungyongshim@nexon.co.kr>
2022-01-03 12:31:23 +0900

    코드 수정

--- src/BlazorApp1/Program.cs
+++ src/BlazorApp1/Program.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Proto;
 using Boost.Proto.Actor.DependencyInjection;
+using BlazorApp1.Actors.Counter;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton(sp => new ActorSystem().Root);
 builder.Services.AddProtoActor(_ => _, _ => _);
+builder.Services.AddSingleton<ProtoActorServiceStart>((sp, root) =>
+{
+    root.SpawnNamed(sp.GetService<IPropsFactory<CounterActor>>().Create(), "CounterActor");
+});
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
