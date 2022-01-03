9097258dfc2f75ebaf67423fb4afe9dd213c1e37
심승용 <seungyongshim@nexon.co.kr>
2022-01-03 09:07:39 +0900

    modified

--- src/BlazorApp1/Program.cs
+++ src/BlazorApp1/Program.cs
@@ -1,6 +1,6 @@
+using BlazorApp1;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Sample.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
